77a0bb95ab492c716557919083b4dd9bdcc8d93e
Gio <kgiovani53@gmail.com>
2025-05-07 20:33:23 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const addTaskBtn = document.querySelector("#add-task");
 const taskList = document.querySelector("#list-tasks");
-//"✔"
+const ids = []
+let id = 0;
+
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];  
 
 addTaskBtn.addEventListener('click', createTask);
 
 function createTask(){
+    id++
+    ids.push(id)
     const li = document.createElement('li');
     li.classList = ("single-task")
+    li.id=(`li${id}`)
     const input = document.createElement("input"); 
     input.type = "text"; 
     input.placeholder = "Digite sua tarefa..."; 
@@ -15,6 +21,14 @@ function createTask(){
     const finishBtn = document.createElement("button");
     finishBtn.textContent = "✔"; 
     finishBtn.classList.add("done-btn");
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "X"; 
+    removeBtn.classList.add("remove-btn");
+    removeBtn.addEventListener('click', (x)=>{
+        idTask = x.target.parentElement.id
+        removeLi(idTask);
+    })
 
     finishBtn.addEventListener("click", () => {
         li.classList.toggle("undone"); 
@@ -25,5 +39,34 @@ function createTask(){
 
     li.appendChild(input);
     li.appendChild(finishBtn);
+    li.appendChild(removeBtn);
     taskList.appendChild(li);
+    
+    function removeLi(id) {
+        const li = document.getElementById(id);
+        li.remove(); 
+        
+      }
+      console.log(taskList);
+
+    updateLocalStorage()
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    tasks.forEach(task => {
+        createTask(task); 
+    });
+});
+
+function updateLocalStorage() {
+    const tasks = [];
+    taskList.querySelectorAll("li").forEach(task => {
+        tasks.push({
+            id: task.id,
+            text: task.querySelector("input").value,
+            done: task.classList.contains("done") 
+        });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks)); 
+}
+console.log(taskList);
